bd1001fff8f53c0bfafdf2a3e6e193fb7134de1c
WonjunJang <wonjun84@naver.com>
2020-07-27 16:11:38 +0900

    Create priority_queue.py

---
+++ priority_queue.py
@@ -0,0 +1,33 @@
+import headq
+
+class PriorityQueue(object):
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, prioirty):
+        headq.heappush(self._queue, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self.queue)[-1]
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return "Item({0!r})".format(self.name)
+
+def test_priority_queue():
+    '''push and pop are all O(logN).'''
+
+    q = PriorityQueue()
+    q.push(Item('test1'), 1)
+    q.push(Item('test2'), 4)
+    q.push(Item('test3'), 3)
+    assert(str(q.pop()) == "Item('test2')")
+    print("Testified!")
+
+if __name__ == "__main__":
+    test_priority_queue()
