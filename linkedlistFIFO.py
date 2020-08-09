2eed9b386c0e54b2d4dff310b80308846305cc1c
WonjunJang <wonjun84@naver.com>
2020-08-09 21:17:42 +0900

    Update linkedlistFIFO.py

--- linkedlistFIFO.py
+++ linkedlistFIFO.py
@@ -13,4 +13,19 @@ class LinkedListFIFO(object):
             print(node.value, end=" ")
             node = node.pointer
         print()
-     
+    
+    # add node in the first place
+    def _addFirst(self, value):
+        self.length = 1
+        node = Node(value)
+        self.head = node
+        self.tail = node
+    
+    # delete first place node
+    def _deleteFirst(self):
+        self.length = 0
+        self.head = None
+        self.tail = None
+        print(" Linked-List is empty.")
+
+    
