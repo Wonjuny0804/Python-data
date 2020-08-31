e948963f0ba75ad722c3a28941ad981f5fb8ab37
WonjunJang <wonjun84@naver.com>
2020-08-31 23:46:46 +0900

    Update palindrome_checkter_woth_deque.py

--- palindrome_checkter_woth_deque.py
+++ palindrome_checkter_woth_deque.py
@@ -3,3 +3,15 @@ import collections
 
 from deque import Deque
 
+STRIP = string.whitespace + string.punctuation + "\"'"
+
+def palindrome_checker_with_deque(str1):
+    d1 = Deque()
+    d2 = collections.deque()
+
+    for s in str1.lower():
+        if s not in STRIP:
+            d2.append(s)
+            d1.enqueue(s)
+
+    
