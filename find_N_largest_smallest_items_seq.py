2ca67f2d34accd359748eb23206214f6dd6c2e56
WonjunJang <wonjun84@naver.com>
2020-09-09 19:25:46 +0900

    Update find_N_largest_smallest_items_seq.py

--- find_N_largest_smallest_items_seq.py
+++ find_N_largest_smallest_items_seq.py
@@ -5,3 +5,21 @@ def find_N_largest_items_seq(seq, N):
 
 def find_N_smallest_items_seq(seq, N):
 	return heapq.nsmallest(N, seq)
+
+def find_smallest_items_seq_heap(seq):
+	heapq.heapify(seq)
+	return heap.q.heappop(seq)
+
+def find_smallest_items_seq(seq):
+	return min(seq)
+
+def find_N_smallest_items_seq_sorted(seq, N):
+	return sorted(seq)[:N]
+
+def find_N_largest_items_seq_sorted(seq, N):
+	return sorted(seq)[len(seq)-N:]
+
+def test_N_largest_smallest_items_seq():
+	seq=[1,3,2,8,6,10,9]
+	N=3
+	assert(find_N_largest_items_seq(seq,N)==[10,9,8])
