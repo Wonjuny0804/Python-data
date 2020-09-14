00c0d4eb89732f2e9591e0985b60f5ef227be542
WonjunJang <wonjun84@naver.com>
2020-09-14 12:42:55 +0900

    Update part_linked_list.py

--- part_linked_list.py
+++ part_linked_list.py
@@ -9,4 +9,35 @@ def partList(ll, n):
 
     while node:
         item = node.value
+
+        if item < n:
+            less.addNode(item)
         
+        elif item > n:
+            more.addNode(item)
+
+        node = node.pointer
+
+    less.addNode(n)
+    nodemore = more.head
+
+    while nodemore:
+        less.addNode(nodemore.value)
+
+        nodemore = nodemore.pointer
+
+    return less
+
+if __name__ == "__main__":
+    ll = LinkedListFIFO()
+    l = [6,7,3,4,9,5,1,2,8]
+    for i in l:
+        ll.addNode(i)
+
+    print("Before:")
+    ll._printList()
+
+    print("After:")
+    newll = partList(11, 6)
+    newll._printList()
+    
