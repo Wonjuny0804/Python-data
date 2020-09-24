1fa400db8240c61b92541dadd72898c5d5d5341f
WonjunJang <wonjun84@naver.com>
2020-09-24 21:56:20 +0900

    Update 14.py

--- DoitCode/14.py
+++ DoitCode/14.py
@@ -5,31 +5,10 @@ Input : aaabbccccca
 Output : a3b2c6a1
 """
 def supress(lists):
-    length = []
-    result = ""
 
-    if len(lists) == 1:
-        return 1
-    
-    for cut in range(1, len(lists) // 2 + 1):
-        count = 1 
-        tempStr = lists[:cut]
-        for i in range(cut, len(lists), cut):
-            if lists[i:i+cut] == tempStr:
-                count += 1
-            else:
-                if count == 1:
-                    count = ""
-                result += str(count) + tempStr
-                tempStr = lists[i:i+cut]
-                count = 1
+    """
+    먼저 생각을 적어보도록 하자. 
+    1) if 
+    """
 
-        if count == 1:
-            count = ""
-        result += str(count) + tempStr
-        length.append(len(lists))
-        result = ""
-
-    return min(length)
-
-print(supress("aaabbbccccc"))
+print(supress("aaabbbccccc")) # 3a3b5c
