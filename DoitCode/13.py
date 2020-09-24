2dc267b79213b205f53c9b6ec0cd7badd347d266
WonjunJang <wonjun84@naver.com>
2020-09-24 12:39:18 +0900

    Update 13.py

--- DoitCode/13.py
+++ DoitCode/13.py
@@ -4,28 +4,20 @@ if there are relays of odd numbers insert "-"
 when even numbers "*"
 
 """
-def DashInsert(numbers):
+def DashInsert(data):
+    numbers = list(map(int, data)) # 숫자 문자열을 숫자 리스트로 변경
+    result = []
 
+    for i, num in enumerate(numbers):
+        result.append(str(num))
+        if i < len(numbers) - 1:
+            is_odd = num % 2 == 1
+            is_next_odd = numbers[i+1] % 2 == 1 
+            if is_odd and is_next_odd:
+                result.append("-")
+            elif not is_odd and is_next_odd:
+                result.append("*")
+    
+    return "".join(result)
 
-    while divide(numbers) == 0,__:
-
-
-def divide(numbers):
-    if len(numbers) == 1:
-        return 0
-    for i in range(1, len(numbers)):
-        if int(numbers[i-1) % 2 == int(numbers[i]) % 2:
-            result = numbers[:i]
-            if int(numbers[i-1]) % 2 == 1: # when ti's odd number
-                result += "-"
-                break
-            else: # when even number
-                result += "*"
-                break
-        
-    return result, numbers[i:]
-haha = "4546793"
-print(DashInsert(haha))
-
-print(3%1)
-print(3/1)
+print(DashInsert("4546793"))
