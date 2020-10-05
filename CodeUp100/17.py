e38392fb0edb3a4fad03ce23732019b6fb28b569
WonjunJang <wonjun84@naver.com>
2020-10-05 15:42:15 +0900

    Create 17.py

---
+++ CodeUp100/17.py
@@ -0,0 +1,13 @@
+"""
+정수 1개 입력받아 3번 출력하기
+int 형 정수 1개를 입력받아 공백을 사이에 두고 
+3번 출력해보자.
+"""
+
+first = int(input())
+# with input() you can add .split()and kind of seperate
+# the data that are coming being input
+
+print(first, first, first)
+# if you put print(A,B) there is automatically a space between
+# A and B!
