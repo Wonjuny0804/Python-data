6dd9328de85ab4fe619d33f92ac3e78edae8e04d
WonjunJang <wonjun84@naver.com>
2020-10-08 15:43:51 +0900

    Create GuessingGame.py

---
+++ GuessingGame/GuessingGame.py
@@ -0,0 +1,29 @@
+"""
+This Game is just a small project that I made for
+fun. You have to guess the number generated between 1 and 1000
+"""
+
+import random
+
+randomnumber = random.randint(1,1000)
+
+lowest = 1
+highest = 1000
+print("Please guess a number between ",lowest,":",highest)
+guess = int(input())
+while True:
+    
+    if guess > randomnumber:
+        highest = guess
+        print("your guess higher go lower")
+        print("Please guess a number between ",lowest,":",highest)
+        guess = int(input())
+    elif guess < randomnumber:
+        lowest = guess
+        print("your guess is lower go higher")
+        print("Please guess a number between",lowest,":",highest)
+        guess = int(input())
+    else:
+        break
+
+print("Game over")
