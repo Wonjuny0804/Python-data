c896514f4f7c014ed4b2a00525800407d26961e4
WonjunJang <wonjun84@naver.com>
2020-10-08 21:50:13 +0900

    Update GuessingGame.py

--- GuessingGame/GuessingGame.py
+++ GuessingGame/GuessingGame.py
@@ -1,14 +1,18 @@
 """
 This Game is just a small project that I made for
 fun. You have to guess the number generated between 1 and 1000
+# v1.1 mild upgrade
+: get the starting number and the end number from the user!
+
 """
 
 import random
 
-randomnumber = random.randint(1,1000)
+start, end = input("Please give us the starting number and the ending number:").split()
+randomnumber = random.randint(int(start),int(end))
 
-lowest = 1
-highest = 1000
+lowest = int(start)
+highest = int(end)
 print("Please guess a number between ",lowest,":",highest)
 guess = int(input())
 while True:
